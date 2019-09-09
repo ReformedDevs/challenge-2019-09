1020ec89c8584617cda2f5a6e6a449c3e3d49c25
Drew Pearce <andrew.f.pearce@accenture.com>
2019-09-09 09:42:38 -0400

    terraform correction

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,6 +2,8 @@ variable "do_token" {}
 variable "pub_key" {}
 variable "pvt_key" {}
 variable "ssh_fingerprint" {}
+variable "user_email" {}
+variable "user_name" {}
 
 provider "digitalocean" {
   token = "${var.do_token}"

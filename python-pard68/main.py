9248b0a6ef1faac83eed2cea89ac134cd25a71f9
Ian Pringle <ipringle2@liberty.edu>
2019-09-12 11:15:17 -0400

    The yeetiest
    - Moved import of `randint` to header, this saved a RIDICULOUS amount of time
    - cleaned up `is_prime()`, removed some redundancy, made it more pythonic, removed a silly logic error
    - switched to using `pow()` for all the exponentiation!

--- python-pard68/main.py
+++ python-pard68/main.py
@@ -1,5 +1,6 @@
 from timeit import default_timer as timer
 from collections import deque
+from random import randint
 
 
 def is_prime(n, t=5):
@@ -7,28 +8,27 @@ def is_prime(n, t=5):
     miller-rabin test
     default to five tests
     '''
-
-    from random import randint
     if n < 2:
         return False
-    for prime in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]:
-        if n % prime == 0:
-            return n == prime
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+
     s, d = 0, n-1
     while d % 2 == 0:
         s, d = int(s+1), int(d/2)
-    for i in range(t):
+
+    for _ in range(t):
         x = pow(randint(2, n-1), d, n)
         if x == 1 or x == n-1:
             continue
-        for r in range(1, s):
-            x = (x * x) % n
-            if x == 1:
-                return False
+        for _ in range(1, s):
+            x = pow(x, 2, n)
             if x == n-1:
                 break
-            else:
-                return False
+        else:
+            return False
         return True
 
 
@@ -48,9 +48,9 @@ def fib(n):
 
 def solution(limit, mask):
     start = timer()
-    n = max([x for x in fib(limit) if x**2 & 0xf == 0x9 if is_prime(x)])
+    n = max([x for x in fib(limit) if pow(x, 2) & 0xf == mask if is_prime(x)])
     end = timer()
-    print(f"pard68, Python 3, {n}, {end - start}, comp_sci++")
+    print(f"pard68, Python 3, {n}, {end - start}, the yeetiest")
 
 
 if __name__ == "__main__":

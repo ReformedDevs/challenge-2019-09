b22e7d91ee9c38be9deb497e0d7fd0fa08ae08fa
Ben Green <ben.green@geckorobotics.com>
2019-09-21 22:39:39 -0400

    Some big optimizations

--- python-pathfinder216/main.py
+++ python-pathfinder216/main.py
@@ -5,30 +5,32 @@ LIMIT = 9000000000000000000
 def square_hexidecimal_ends_with_9(number):
     return (number * number) & 0b1111 == 0x9
 
-def get_prime_fibonacci_numbers_under(limit):
+def get_special_fibonacci_numbers_under(limit):
     numbers = []
-    previous = 0
-    current = 1
+    previous, current = 1, 1
     while current < limit:
-        if is_prime(current):
+        if square_hexidecimal_ends_with_9(current):
             numbers.append(current)
-        current += previous
-        previous = current - previous
+        previous, current = current, previous + current
 
     return numbers
 
 def is_prime(number):
     """ Assumes number is greater than 1 """
-    return all(number % num for num in range(2, int(number**0.5) + 1))
+    if not number % 2:
+        return False
+    return all(number % num for num in range(3, int(number**0.5) + 1, 2))
+
+def find_answer_search_fibonacci():
+    fibonacci_numbers = get_special_fibonacci_numbers_under(LIMIT)
+    for number in reversed(fibonacci_numbers):
+        if square_hexidecimal_ends_with_9(number) and is_prime(number):
+            return number
 
 if __name__ == '__main__':
     start = time()
-
-    prime_fibonacci_numbers = get_prime_fibonacci_numbers_under(LIMIT)
-    for number in reversed(prime_fibonacci_numbers):
-        if square_hexidecimal_ends_with_9(number):
-            print(f'Found the answer: {number}')
-            break
-
+    answer = find_answer_search_fibonacci()
     end = time()
+
+    print(f'Found the answer: {answer}')
     print(f'Finished in: {end - start} seconds')

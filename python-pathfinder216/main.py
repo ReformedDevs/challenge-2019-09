ef2ccda4f68b03239cef3c466460e06b1bec97c2
Ben Green <ben.green@geckorobotics.com>
2019-09-21 23:36:43 -0400

    Comments and minor refactoring

--- python-pathfinder216/main.py
+++ python-pathfinder216/main.py
@@ -4,9 +4,14 @@ from timeit import default_timer
 LIMIT = 9000000000000000000
 
 def square_hexidecimal_ends_with_9(number):
-    return (number * number) & 0b1111 == 0x9
+    """ Return whether the given number's square ends with 0x9 """
+    return (number * number) & 0xf == 0x9
 
 def get_special_fibonacci_numbers_under(limit):
+    """
+    Return a list of fibonacci numbers whose square ends with 0x9
+    The list stops at the given limit
+    """
     numbers = []
     previous, current = 1, 1
     while current < limit:
@@ -17,12 +22,13 @@ def get_special_fibonacci_numbers_under(limit):
     return numbers
 
 def is_prime(number):
-    """ Assumes number is greater than 1 """
-    if not number % 2:
+    """ Returns whether the given number is prime """
+    if number <= 1 or not number % 2:
         return False
     return all(number % num for num in range(3, int(number**0.5) + 1, 2))
 
 def find_answer_search_fibonacci():
+    """ Count up fibonacci numbers to find the answer """
     fibonacci_numbers = get_special_fibonacci_numbers_under(LIMIT)
     for number in reversed(fibonacci_numbers):
         if square_hexidecimal_ends_with_9(number) and is_prime(number):
